0ccfdf58b20c7a4c65f4ef35dbad5b7ceaf832ab
Lailla-ruh <lailla.ruhanny@academico.ifpb.edu.br>
2026-04-15 10:52:29 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("fotolailla.png")
+st.write("Site Lailla")
+st.link_button("Acessar", "https://sites.google.com/d/19rZhooAY66aV-K0EKjCHUDInYhGcLZdV/p/1RbMTpxwYMKlFabiPhgpLbYpQQL6Rrco1/edit")
