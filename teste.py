7817d6962ed4974640d862cd460119d79f382889
Lailla-ruh <lailla.ruhanny@academico.ifpb.edu.br>
2026-04-15 10:53:32 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("fotolailla.png")
+st.image("fotolailla.jpeg")
 st.write("Site Lailla")
 st.link_button("Acessar", "https://sites.google.com/d/19rZhooAY66aV-K0EKjCHUDInYhGcLZdV/p/1RbMTpxwYMKlFabiPhgpLbYpQQL6Rrco1/edit")
